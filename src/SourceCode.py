a891bfb1bcb574c455de81f7b0e3bb2b3afd5fd0
evertonbrunosds <37885125+evertonbrunosds@users.noreply.github.com>
2020-05-11 23:09:45 -0300

    Censo Demografico

--- src/SourceCode.py
+++ src/SourceCode.py
@@ -1,13 +1,7 @@
 '''
 /*******************************************************************************
 Autor: Everton Bruno Silva dos Santos
-Componente Curricular: MI - ALGORITMOS
 Concluido em:04/08/2019
-Declaro que este código foi elaborado por mim de forma individual e não contém nenhum
-trecho de código de outro colega ou de outro autor, tais como provindos de livros e
-apostilas e páginas ou documentos eletrônicos da Internet. Qualquer trecho de código
-de outra autoria que não a minha está destacado com uma citação para o autor e a fonte
-do código e estou ciente que estes trechos não serão considerados para fins de avaliação.
 /*******************************************************************************
 '''
 import os #BIBLIOTECA QUE HABILITA O USO DE COMANDO EXTERNOS
